4e3d9c9a4808bf36490672677371db5aee3629c3
Andrii Dudar <andriidudar@Andriis-MBP.fritz.box>
2023-11-11 15:33:50 +0100

    add aws prod config

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ const pgClient = new Pool({
     database: keys.pgDatabase,
     password: keys.pgPassword,
     port: keys.pgPort,
-    ssl: false,
+    ssl: {rejectUnauthorized: false }
 });
 
 pgClient.on('error', () => console.log('Lost PG connection'));
